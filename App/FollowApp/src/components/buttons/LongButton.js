943d1fd9851691f813a17ff2efaf18012b6fee24
pengm2ng <cjksong@ajou.ac.kr>
2021-08-03 23:25:36 +0900

    Fix button component

--- App/FollowApp/src/components/buttons/LongButton.js
+++ App/FollowApp/src/components/buttons/LongButton.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import styled from 'styled-components/native'
-import {Dimensions} from 'react-native'
+import React from 'react';
+import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
 
-const WindowHeight =Dimensions.get('window').width - 100
+const WindowHeight = Dimensions.get('window').width - 100;
 const ButtonContainer = styled.TouchableOpacity`
-
-  border-radius: 20px;
-  align-items: center;
-  padding: 10px 50px;
-  margin: 10px 0px;
-  justify-content: center;
-  background-color: #000000; 
-  width: ${WindowHeight}px;
-
+    border-radius: 20px;
+    align-items: center;
+    padding: 10px 50px;
+    margin: 10px 0px;
+    justify-content: center;
+    background-color: #000000;
+    width: ${WindowHeight}px;
 `;
 
 const Title = styled.Text`
@@ -21,13 +19,12 @@ const Title = styled.Text`
     font-weight: 600;
 `;
 
-
-const LongButton = (props) => {
-    return(
+const LongButton = props => {
+    return (
         <ButtonContainer activeOpacity={0.8} onPress={props.onPress}>
             <Title>{props.title}</Title>
         </ButtonContainer>
-    )
-}
+    );
+};
 
 export default LongButton;
--- App/FollowApp/src/components/buttons/ShortButton.js
+++ App/FollowApp/src/components/buttons/ShortButton.js
@@ -1,34 +1,30 @@
-import React from 'react'
-import styled from 'styled-components/native'
-import {Dimensions} from 'react-native'
+import React from 'react';
+import styled from 'styled-components/native';
+import {Dimensions} from 'react-native';
 
-const WindowHeight =Dimensions.get('window').width - 320
+const WindowHeight = Dimensions.get('window').width - 320;
 const ButtonContainer = styled.TouchableOpacity`
-
-  border-radius: 20px;
-  align-items: center;
-  padding: 5px 20px;
-  margin: 10px 0px;
-  justify-content: center;
-  background-color: #000000; 
-  width: ${WindowHeight}px;
-
+    border-radius: 20px;
+    align-items: center;
+    padding: 5px 20px;
+    margin: 10px 0px;
+    justify-content: center;
+    background-color: #000000;
+    width: ${WindowHeight}px;
 `;
 
 const Title = styled.Text`
-
     font-size: 13px;
     font-weight: 600;
     color: #fff;
-    
 `;
 
 const ShortButton = props => {
-    return(
-        <ButtonContainer  activeOpacity={0.8} onPress={props.onPress}>
+    return (
+        <ButtonContainer activeOpacity={0.8} onPress={props.onPress}>
             <Title>{props.title}</Title>
         </ButtonContainer>
-    )
-}
+    );
+};
 
 export default ShortButton;
