5074250d8690d19d63d7aef5e65f74eb6659aad4
pengm2ng <cjksong@ajou.ac.kr>
2021-08-04 16:46:14 +0900

    add bottom sheet modal on tryon

--- App/FollowApp/src/screens/MyCloset/TryOnScreen.js
+++ App/FollowApp/src/screens/MyCloset/TryOnScreen.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Dimensions} from 'react-native';
 import styled from 'styled-components/native';
-import Button from '../../components/buttons/Button';
+import Buttons from '../../components/buttons/Button';
+import MyClothesBottomSheet from '../../components/bottomSheet/MyClothesButtomSheet';
+import MyLooksBottomSheet from '../../components/bottomSheet/MyLooksButtomSheet';
 
 const Container = styled.SafeAreaView`
     flex: 1;
     align-items: center;
-    justify-content: center;
 `;
 const AvatarView = styled.View`
-    flex: 1;
+    border-radius: 20px;
     align-items: center;
     justify-content: center;
+    width: 90%;
+    height: 50%;
     border: 1px;
+    margin-bottom: 20%;
+    margin-top: 10%;
 `;
 
-const TryOnScreen = () => {
+const TryOnScreen = ({props}) => {
+    const [modalOneVisible, setModalOneVisible] = useState(false);
+    const [modalTwoVisible, setModalTwoVisible] = useState(false);
+    const pressMyClothesButton = () => {
+        setModalOneVisible(true);
+    };
+    const pressMyLooksButton = () => {
+        setModalTwoVisible(true);
+    };
     return (
         <Container>
-            <Button.LongButton title="My Clothes" />
-            <Button.LongButton title="My Looks" />
+            <AvatarView></AvatarView>
+            <Buttons.LongButton
+                title="My Clothes"
+                onPress={pressMyClothesButton}
+            />
+            <MyClothesBottomSheet
+                modalVisible={modalOneVisible}
+                setModalVisible={setModalOneVisible}
+            />
+            <Buttons.LongButton title="My Looks" onPress={pressMyLooksButton} />
+            <MyLooksBottomSheet
+                modalVisible={modalTwoVisible}
+                setModalVisible={setModalTwoVisible}
+            />
         </Container>
     );
 };
