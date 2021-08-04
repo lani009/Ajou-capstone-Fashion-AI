bf053bf7ac69591abddd282bb3e458f5723eafd3
pengm2ng <cjksong@ajou.ac.kr>
2021-08-04 15:46:47 +0900

    tryOn frame

--- App/FollowApp/src/screens/MyCloset/TryOnScreen.js
+++ App/FollowApp/src/screens/MyCloset/TryOnScreen.js
@@ -1,5 +1,26 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import Button from '../../components/buttons/Button';
+
+const Container = styled.SafeAreaView`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+`;
+const AvatarView = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    border: 1px;
+`;
+
 const TryOnScreen = () => {
-    return null;
+    return (
+        <Container>
+            <Button.LongButton title="My Clothes" />
+            <Button.LongButton title="My Looks" />
+        </Container>
+    );
 };
 
 export default TryOnScreen;
