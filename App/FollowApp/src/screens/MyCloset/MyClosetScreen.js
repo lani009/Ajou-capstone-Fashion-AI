1c60579df6a8ac2d6e2d37bd9cfdf594bfc8b90d
JuHwan_Park <standardjuhwan@gmail.com>
2021-08-11 22:17:32 +0900

    사진추가 및 버튼 생성전까지 완성

--- App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
+++ App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Image, Text, Button} from 'react-native';
+import MyCloth from './MyCloth';
+import MyLookScreen from './MyLookScreen';
+import MyAvatarScreen from './MyAvatar';
+import TryOnScreen from './TryOnScreen';
+import Navigation from '../../navigations/navigation';
+
 
 const MyClosetScreen = () => {
     return (
@@ -7,12 +13,35 @@ const MyClosetScreen = () => {
             <View
                 style={{width: 400, height: 150, backgroundColor: 'skyblue'}}
             />
+            <Image
+                source={require('./Image/12.png')}
+                style= {styles.container}
+            />
+            <Text
+                style= {{marginTop: 60, marginLeft:170, fontWeight: 'bold', fontSize: 20, color: 'steelblue'}}>
+                주돌이
+            </Text>
+            <Text
+                style= {{marginTop: 10, marginLeft:100, fontWeight: 'normal', fontSize: 15, color: 'steelblue'}}>
+                jkworldchampion@gamil.com
+            </Text>
+
+            <Button
+                title="Press me"
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {},
+    container: { 
+        width: 110,
+        height: 110,
+        position: 'absolute',
+        marginTop: 100,
+        marginLeft: 140
+        
+    },
 });
 
 export default MyClosetScreen;
