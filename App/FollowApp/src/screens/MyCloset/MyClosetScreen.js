e3e3a3af3f99868d34d17c82e990ed6de3e0c528
JuHwan_Park <standardjuhwan@gmail.com>
2021-08-11 17:25:14 +0900

    구동 확인 및 화면 추가

--- App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
+++ App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
 const MyClosetScreen = () => {
-    return null;
+    return (
+        <View>
+            <View
+                style={{width: 400, height: 150, backgroundColor: 'skyblue'}}
+            />
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {},
+});
 
 export default MyClosetScreen;
