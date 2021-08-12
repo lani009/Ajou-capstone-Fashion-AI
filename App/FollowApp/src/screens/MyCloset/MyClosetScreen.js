b9032f6bd84bebe1c3176d8b4eeccc5d0608e88b
JuHwan_Park <standardjuhwan@gmail.com>
2021-08-12 13:46:01 +0900

    상대적 위치로  수정사항 변경

--- App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
+++ App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
@@ -4,7 +4,7 @@ import {StyleSheet, View, Image, Text, TouchableOpacity} from 'react-native';
 function MyClosetScreen({navigation}) {
     return (
         <View>
-            <View style={{width: 400, height: 150, backgroundColor: 'black'}} />
+            <View style={styles.upbox} />
 
             <Image
                 source={require('./Image/12.png')}
@@ -14,7 +14,7 @@ function MyClosetScreen({navigation}) {
             <Text
                 style={{
                     marginTop: 60,
-                    marginLeft: 170,
+                    textAlign: 'center',
                     fontWeight: 'bold',
                     fontSize: 20,
                     color: 'black',
@@ -25,14 +25,14 @@ function MyClosetScreen({navigation}) {
             <Text
                 style={{
                     marginTop: 10,
-                    marginLeft: 100,
+                    textAlign: 'center',
                     fontWeight: 'normal',
                     fontSize: 15,
                     color: 'black',
                 }}>
                 jkworldchampion@gamil.com
             </Text>
-            <View style={{flexDirection: 'row'}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
                 <TouchableOpacity style={styles.BoxSutton}
                     onPress={() => navigation.navigate('MyClothes')}>
                     <Text
@@ -56,7 +56,7 @@ function MyClosetScreen({navigation}) {
                     </Text>
                 </TouchableOpacity>
             </View>
-            <View style={{flexDirection: 'row'}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
                 <TouchableOpacity
                     style={styles.BoxSutton}
                     onPress={() => navigation.navigate('MyAvatar')}>
@@ -99,10 +99,14 @@ const styles = StyleSheet.create({
         padding: 40,
         marginTop: 40,
         borderRadius: 13,
-        marginLeft: 30,
         width: 150,
         height: 150,
     },
+    upbox: {
+        width: '100%',
+        height: 150,
+        backgroundColor: 'black',
+    }
 });
 
 export default MyClosetScreen;
