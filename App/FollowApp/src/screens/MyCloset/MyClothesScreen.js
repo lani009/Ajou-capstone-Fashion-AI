3d92a664606736e160ea01935cab2304fb7b4c6d
pengm2ng <cjksong@ajou.ac.kr>
2021-08-14 20:43:15 +0900

    myClothesScreen 완료 -> 스크롤 기능 없음.  화면크기 맞는지 확인바람

--- App/FollowApp/src/screens/MyCloset/MyClothesScreen.js
+++ App/FollowApp/src/screens/MyCloset/MyClothesScreen.js
@@ -2,14 +2,16 @@ import React, {useState} from 'react';
 import {View} from 'react-native';
 import styled from 'styled-components/native';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
-
+import {MyClosetProvider} from '../../context/MyClosetContext';
+import AllClothesImageLoader from '../../components/MyClothesScreen/AllClothesContextLoader';
+import TypeContextLoader from '../../components/MyClothesScreen/TypeContextLoader';
+import ColorContextLoader from '../../components/MyClothesScreen/ColorContextLoader';
 const Container = styled.SafeAreaView`
     flex: 1;
     align-items: center;
 `;
 
 const ClothesContainer = styled.View``;
-const ScrollView = styled.ScrollView``;
 
 const MyClothesScreen = () => {
     const [selectedIndex, setSelectedIndex] = useState(0);
@@ -18,21 +20,25 @@ const MyClothesScreen = () => {
         setSelectedIndex(index);
     };
     return (
-        <Container>
-            <View style={{width: '70%', margin: '5%'}}>
-                <SegmentedControlTab
-                    values={['Type', 'Color']}
-                    selectedIndex={selectedIndex}
-                    tabStyle={{borderColor: 'black'}}
-                    activeTabStyle={{backgroundColor: 'black'}}
-                    onTabPress={handleSingleIndexSelect}
-                    tabTextStyle={{color: 'black'}}
-                />
-            </View>
-            <ScrollView>
-                <ClothesContainer></ClothesContainer>
-            </ScrollView>
-        </Container>
+        <MyClosetProvider>
+            <Container>
+                <View style={{width: '80%', margin: '5%'}}>
+                    <SegmentedControlTab
+                        values={['All', 'Type', 'Color']}
+                        selectedIndex={selectedIndex}
+                        tabStyle={{borderColor: 'black'}}
+                        activeTabStyle={{backgroundColor: 'black'}}
+                        onTabPress={handleSingleIndexSelect}
+                        tabTextStyle={{color: 'black'}}
+                    />
+                </View>
+                <ClothesContainer>
+                    {selectedIndex === 0 && <AllClothesImageLoader />}
+                    {selectedIndex === 1 && <TypeContextLoader />}
+                    {selectedIndex === 2 && <ColorContextLoader />}
+                </ClothesContainer>
+            </Container>
+        </MyClosetProvider>
     );
 };
 export default MyClothesScreen;
