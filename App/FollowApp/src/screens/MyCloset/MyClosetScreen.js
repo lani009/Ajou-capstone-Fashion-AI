7ab2f710ca356e3e38d39f33fd64c2d95d59444e
JK-legend <standardjuhwan@gmail.com>
2021-08-23 00:05:25 +0900

    못 끝내서..쏴리

--- App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
+++ App/FollowApp/src/screens/MyCloset/MyClosetScreen.js
@@ -93,7 +93,7 @@ const styles = StyleSheet.create({
     },
     upbox: {
         width: '100%',
-        height: 150,
+        height: '10%',
         backgroundColor: 'black',
     },
     Boxtext: {
